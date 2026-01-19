19f48960031e0abb5c2a1fe9bf876758b67438c2
Jayithape <jayithape76@gmail.com>
2026-01-19 08:48:32 +0530

    Create Student.java

---
+++ backend/src/main/java/com/text/entities/Student.java
@@ -0,0 +1,45 @@
+package com.text.entities;
+
+import org.springframework.stereotype.Component;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private int age;
+    private String name;
+
+    // 🔥 THIS IS MUST
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
+
